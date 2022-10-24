bb2cc9bc72466048bba8f417e15b2dad1b0f3683
limecats <limecats0331@gmail.com>
2022-10-24 13:32:57 +0900

    make initial project

---
+++ BackEnd/ssamuso/src/main/java/com/ssafy/ssamuso/SsamusoApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.ssamuso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsamusoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsamusoApplication.class, args);
+	}
+
+}
---
+++ BackEnd/ssamuso/src/test/java/com/ssafy/ssamuso/SsamusoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.ssamuso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsamusoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
