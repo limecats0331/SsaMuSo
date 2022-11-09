5ebece67288cb714498faa6db78f2cc94ee01be4
limecats <limecats0331@gmail.com>
2022-11-09 14:46:41 +0900

    fix(BE): TeammateInfoDTO 수정
    list 필드 초기화

--- BackEnd/ssamuso/src/main/java/com/ssafy/ssamuso/domain/dto/TeammateInfoDTO.java
+++ BackEnd/ssamuso/src/main/java/com/ssafy/ssamuso/domain/dto/TeammateInfoDTO.java
@@ -4,13 +4,14 @@ import com.ssafy.ssamuso.domain.entity.enumtype.TechName;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
 @Builder
 public class TeammateInfoDTO {
     private String username;
-    private List<TechName> techstacks;
+    private List<TechName> techstacks = new ArrayList<>();
     private String portfoliosLink;
 
     protected TeammateInfoDTO() {
