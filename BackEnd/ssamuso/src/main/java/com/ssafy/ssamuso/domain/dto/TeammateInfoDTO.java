4cecb3cb740ff080063b70ac64d97b951d230411
limecats <limecats0331@gmail.com>
2022-11-09 14:55:10 +0900

    feat(BE): findTeammateByUsername 추가
    서비스에 추가

--- BackEnd/ssamuso/src/main/java/com/ssafy/ssamuso/domain/dto/TeammateInfoDTO.java
+++ BackEnd/ssamuso/src/main/java/com/ssafy/ssamuso/domain/dto/TeammateInfoDTO.java
@@ -1,19 +1,14 @@
 package com.ssafy.ssamuso.domain.dto;
 
 import com.ssafy.ssamuso.domain.entity.enumtype.TechName;
-import lombok.Builder;
 import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
-@Builder
 public class TeammateInfoDTO {
     private String username;
     private List<TechName> techstacks = new ArrayList<>();
     private String portfoliosLink;
-
-    protected TeammateInfoDTO() {
-    }
 }
--- BackEnd/ssamuso/src/main/java/com/ssafy/ssamuso/service/UserServiceImlp.java
+++ BackEnd/ssamuso/src/main/java/com/ssafy/ssamuso/service/UserServiceImlp.java
@@ -1,22 +1,54 @@
 package com.ssafy.ssamuso.service;
 
+import com.ssafy.ssamuso.domain.dto.TeammateInfoDTO;
+import com.ssafy.ssamuso.domain.entity.Portfolios;
 import com.ssafy.ssamuso.domain.entity.User;
+import com.ssafy.ssamuso.domain.entity.UserTechstack;
+import com.ssafy.ssamuso.repository.PortfoliosRepository;
 import com.ssafy.ssamuso.repository.UserRepository;
+import com.ssafy.ssamuso.repository.UserTechstackRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImlp {
     private final UserRepository userRepository;
+    private final PortfoliosRepository portfoliosRepository;
+    private final UserTechstackRepository userTechstackRepository;
 
     Optional<User> findById(Long userId) {
         return userRepository.findById(userId);
     }
 
-    public Optional<User> findByUsername(String name){
+    public Optional<User> findByUsername(String name) {
         return userRepository.findByUsername(name);
+    }
+
+    public Optional<TeammateInfoDTO> findTeammateByUsername(String username) {
+        Optional<User> findUser = userRepository.findByUsername(username);
+        if (findUser.isEmpty()) {
+            return null;
+        }
+
+        Optional<Portfolios> findPortfolios = portfoliosRepository.findByUser(findUser.get());
+        Optional<List<UserTechstack>> findUserTeckstacks = userTechstackRepository.findAllByUser(findUser.get());
+
+        TeammateInfoDTO teammateInfoDTO = new TeammateInfoDTO();
+
+        teammateInfoDTO.setUsername(findUser.get().getUsername());
+        if (findPortfolios.isPresent()) {
+            teammateInfoDTO.setPortfoliosLink(findPortfolios.get().getLink());
+        }
+        if (findUserTeckstacks.isPresent()) {
+            for (UserTechstack userTechstack : findUserTeckstacks.get()) {
+                teammateInfoDTO.getTechstacks().add(userTechstack.getTechstack().getTechName());
+            }
+        }
+
+        return Optional.of(teammateInfoDTO);
     }
 }
