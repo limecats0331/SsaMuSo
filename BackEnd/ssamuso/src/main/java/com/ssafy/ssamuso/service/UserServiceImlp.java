ff63ccda9928869c221e7e8133cf44579fbce2ac
limecats0331 <limecats0331@gmail.com>
2022-11-11 10:01:38 +0900

    feat(BE): 서비스에 유저 삭제 추가

--- BackEnd/ssamuso/src/main/java/com/ssafy/ssamuso/service/UserServiceImlp.java
+++ BackEnd/ssamuso/src/main/java/com/ssafy/ssamuso/service/UserServiceImlp.java
@@ -1,10 +1,13 @@
 package com.ssafy.ssamuso.service;
 
+import com.amazonaws.services.ec2.model.UserData;
 import com.ssafy.ssamuso.domain.dto.TeammateInfoDTO;
 import com.ssafy.ssamuso.domain.entity.Portfolios;
 import com.ssafy.ssamuso.domain.entity.User;
+import com.ssafy.ssamuso.domain.entity.UserDelete;
 import com.ssafy.ssamuso.domain.entity.UserTechstack;
 import com.ssafy.ssamuso.repository.PortfoliosRepository;
+import com.ssafy.ssamuso.repository.UserDeleteRepository;
 import com.ssafy.ssamuso.repository.UserRepository;
 import com.ssafy.ssamuso.repository.UserTechstackRepository;
 import lombok.RequiredArgsConstructor;
@@ -19,6 +22,7 @@ public class UserServiceImlp {
     private final UserRepository userRepository;
     private final PortfoliosRepository portfoliosRepository;
     private final UserTechstackRepository userTechstackRepository;
+    private final UserDeleteRepository userDeleteRepository;
 
     Optional<User> findById(Long userId) {
         return userRepository.findById(userId);
@@ -50,5 +54,28 @@ public class UserServiceImlp {
         }
 
         return Optional.of(teammateInfoDTO);
+    }
+
+    private void deleteUser(User user) {
+        UserDelete userDelete = makeDeleteUser(user);
+        userDeleteRepository.save(userDelete);
+        userRepository.deleteById(user.getId());
+    }
+
+    private UserDelete makeDeleteUser(User user) {
+         return UserDelete.builder()
+                .id(user.getId())
+                .area(user.getArea())
+                .track(user.getTrack())
+                .password(user.getPassword())
+                .userTechstacks(user.getUserTechstacks())
+                .email(user.getEmail())
+                .profileImg(user.getProfileImg())
+                .term(user.getTerm())
+                .classNum(user.getClassNum())
+                .role(user.getRole())
+                .name(user.getName())
+                .username(user.getUsername())
+                .build();
     }
 }
