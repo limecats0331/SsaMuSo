b03c76777213d8f180b9d9dd5f5d38f14ff99dca
limecats0331 <limecats0331@gmail.com>
2022-11-11 09:56:00 +0900

    fix(BE): 입력받는 엔티티 수정

--- BackEnd/ssamuso/src/main/java/com/ssafy/ssamuso/repository/UserDeleteRepository.java
+++ BackEnd/ssamuso/src/main/java/com/ssafy/ssamuso/repository/UserDeleteRepository.java
@@ -1,11 +1,10 @@
 package com.ssafy.ssamuso.repository;
 
-import com.ssafy.ssamuso.domain.entity.User;
 import com.ssafy.ssamuso.domain.entity.UserDelete;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserDeleteRepository extends JpaRepository<UserDelete, Long> {
-    UserDelete save(User user);
+    UserDelete save(UserDelete userDelete);
 }
