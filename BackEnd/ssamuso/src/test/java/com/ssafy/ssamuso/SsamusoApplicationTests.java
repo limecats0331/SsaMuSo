2b03fc22ef71b978f68ea7e98d2dd51c1abddb7c
qgw13467 <qgw13467@naver.com>
2022-11-15 10:30:41 +0900

    Revert "Revert "feat(BE): 테스트 코드 수정""
    This reverts commit a5f137a02fbd325074e7975d60cc80a89331953f.

--- BackEnd/ssamuso/src/test/java/com/ssafy/ssamuso/SsamusoApplicationTests.java
+++ BackEnd/ssamuso/src/test/java/com/ssafy/ssamuso/SsamusoApplicationTests.java
@@ -3,10 +3,10 @@ package com.ssafy.ssamuso;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SsamusoApplicationTests {
 
-	@Test
+//	@Test
 	void contextLoads() {
 
 	}
