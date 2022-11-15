b8a710589c45de92bfc111a1844ce2febf8351c3
qgw13467 <qgw13467@naver.com>
2022-11-15 10:24:11 +0900

    feat(BE): ModelMapperConfig 추가

--- BackEnd/ssamuso/src/main/java/com/ssafy/ssamuso/config/CustomModelMapper.java
+++ BackEnd/ssamuso/src/main/java/com/ssafy/ssamuso/config/CustomModelMapper.java
@@ -8,30 +8,9 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class CustomModelMapper{
 
-    private final ModelMapper modelMapper = new ModelMapper();
-
     @Bean
-    public ModelMapper strictMapper() {
-        // 매핑 전략 설정
-        modelMapper.getConfiguration()
-                .setMatchingStrategy(MatchingStrategies.STRICT);
-        return modelMapper;
-    }
-
-    @Bean
-    public ModelMapper standardMapper() {
-        // 매핑 전략 설정
-        modelMapper.getConfiguration()
-                .setMatchingStrategy(MatchingStrategies.STANDARD);
-        return modelMapper;
-    }
-
-    @Bean
-    public ModelMapper looseMapper() {
-        // 매핑 전략 설정
-        modelMapper.getConfiguration()
-                .setMatchingStrategy(MatchingStrategies.LOOSE);
-        return modelMapper;
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
     }
 
 }
